d33ae23e6bdaad18d9051dcd29656e7c55f8e0f4
hamidreza <72hamidhasani@gmail.com>
2025-10-16 17:28:19 +0000

    قبل از rebase: ذخیره تغییرات محلی

--- nadinsoftTask/app.py
+++ nadinsoftTask/app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
+import time
+
 app = Flask(__name__)
+REQUESTS = Counter("app_requests_total", "Total HTTP requests", ["endpoint"])
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Hello from NadinSoft Flask App!"
+    REQUESTS.labels(endpoint="/").inc()
+    return "Hello from Dockerized Flask App with Metrics!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/healthz")
+def healthz():
+    REQUESTS.labels(endpoint="/healthz").inc()
+    return jsonify(status="ok", ts=time.time())
+
+@app.route("/metrics")
+def metrics():
+    return generate_latest(), 200, {"Content-Type": CONTENT_TYPE_LATEST}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
